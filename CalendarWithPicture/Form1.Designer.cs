31f0ce0bc0be50ce889ca6c2199dfdb11e7474c9
N1ikHard <N1ik@yandex.ru>
2021-04-19 15:26:28 +0300

    Добавьте файлы проекта.

---
+++ CalendarWithPicture/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace CalendarWithPicture
+{
+    partial class MyCalendar
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar
+            // 
+            this.monthCalendar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.monthCalendar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monthCalendar.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar.Name = "monthCalendar";
+            this.monthCalendar.ShowToday = false;
+            this.monthCalendar.TabIndex = 1;
+            this.monthCalendar.TitleBackColor = System.Drawing.Color.Transparent;
+            this.monthCalendar.TitleForeColor = System.Drawing.Color.Transparent;
+            this.monthCalendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar_DateChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(792, 513);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // MyCalendar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 513);
+            this.Controls.Add(this.monthCalendar);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "MyCalendar";
+            this.Text = "12 Month";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ CalendarWithPicture/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalendarWithPicture
+{
+    public partial class MyCalendar : Form
+    {
+        public MyCalendar()
+        {
+            InitializeComponent();
+            ShowPicture();
+        }
+
+        private void monthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            ShowPicture();
+        }
+        void ShowPicture()
+        {
+            try 
+            {
+                pictureBox.BackgroundImageLayout = ImageLayout.Stretch;
+                pictureBox.Image = Image.FromFile("12 Month\\" + monthCalendar.SelectionStart.Month + ".jpg");
+                pictureBox.BackgroundImageLayout = ImageLayout.Center;
+            }
+            catch
+            {
+                MessageBox.Show("Изображения не найдены!", "Ошибка",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            }
+    }
+}
